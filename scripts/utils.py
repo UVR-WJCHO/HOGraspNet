bdab02472406be70eb621960c47c877e8515857a
UVR-WJCHO <woojin.cho@kaist.ac.kr>
2024-08-12 14:57:12 +0900

    .

--- scripts/utils.py
+++ scripts/utils.py
@@ -102,7 +102,9 @@ def download_urls(urls, output_folder, max_tries=7):
 
 def download_url_once(url, filename, progress=True):
     try:
-        headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
+        # headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
+        headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+
         r = requests.get(url, stream=True, proxies=None, headers=headers)
     except ConnectionError as err:
         print(err)
