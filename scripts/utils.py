b0a08ecea950f9fb7eac9abca9214c83f858ecb3
UVR-WJCHO <woojin.cho@kaist.ac.kr>
2024-08-12 14:11:16 +0900

    .

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,11 @@
 import os
 import wget
 from config import cfg
+import requests
+from requests.exceptions import ConnectionError
+from tqdm.autonotebook import tqdm
+import time
+import math
 
 
 def extractBbox(hand_2d, image_rows=1080, image_cols=1920, bbox_w=640, bbox_h=480):
@@ -62,9 +67,81 @@ def check_args(arg_type, arg_subject):
     return target_url_set, subjects
 
 
-def download_urls(urls, output_folder):
+def download_urls(urls, output_folder, max_tries=7):
+    """
+    Download file from a URL to file_name,
+    source refer from https://github.com/facebookresearch/ContactPose/blob/main/utilities/networking.py
+    """
     for url in urls:
         file_name = url.split('/')[-1].split('?')[0]
+
+        print(f"Downloading file name : {file_name}")
+
         file_name = output_folder + '/' + file_name
-        # url = url[:-1] + '1'
-        os.system(f"wget -cO {file_name} \"{url}\"")
+        url = url[:-1] + '1'
+
+        # os.system(f"wget -cO {file_name} \"{url}\"")
+        # os.system(f"curl -L -o {file_name} \"{url}\"")
+
+        ## from contactpose
+        tries = 0
+        while tries < max_tries:
+            done = download_url_once(url, file_name, True)
+            if not done:
+                t = exponential_backoff(tries)
+                print('*** Sleeping for {:f} s'.format(t))
+                time.sleep(t)
+                tries += 1
+        print('*** Max download tries exceeded')
+
+
+def exponential_backoff(n, max_backoff=64.0):
+    t = math.pow(2.0, n)
+    t += (random.randint(0, 1000)) / 1000.0
+    t = min(t, max_backoff)
+    return t
+
+
+def download_url_once(url, filename, progress=True):
+    try:
+        r = requests.get(url, stream=True, proxies=None)
+    except ConnectionError as err:
+        print(err)
+        return False
+    
+    total_size = int(r.headers.get('content-length', 0))
+    # print("total_size : ", total_size)
+    block_size = 1024 #1 Kibibyte
+    if progress:
+        t=tqdm(total=total_size, unit='iB', unit_scale=True)
+    done = True
+    datalen = 0
+    with open(filename, 'wb') as f:
+        itr = r.iter_content(block_size)
+        while True:
+            try:
+                try:
+                    data = next(itr)
+                except StopIteration:
+                    break
+                if progress:
+                    t.update(len(data))
+                datalen += len(data)
+                f.write(data)
+            except KeyboardInterrupt:
+                done = False
+                print('Cancelled')
+            except ConnectionError as err:
+                done = False
+                print(err)
+    if progress:
+        t.close()
+    if (not done) or (total_size != 0 and datalen != total_size):
+        print("ERROR, something went wrong")
+        try:
+            os.remove(filename)
+        except OSError as e:
+            print(e)
+        return False
+    else:
+        return True
